705bfbde5c3f9a3517d7358644e31c377b3199fa
Moxvallix <moxvallix@gmail.com>
2022-05-18 12:05:23 +0930

    Revert "fix: swap system method for exec, in the cli"
    This reverts commit 5f6ed5d9648ea1011c25e6370c2e9f4c797d4394.

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,26 +4,26 @@ require "thor"
 class DeckerCLI < Thor
   desc "install [PACKAGE]", "installs PACKAGE and registers it to Decker"
   def install(package)
-    exec("babushka.rb 'current dir:install' package_name=#{package}")
+    system("babushka.rb 'current dir:install' package_name=#{package}")
   end
 
   desc "remove [PACKAGE]", "removes PACKAGE and it's unique dependencies, from the system and from Decker"
   def remove(package)
-    exec("babushka.rb 'current dir:remove' package_name=#{package}")
+    system("babushka.rb 'current dir:remove' package_name=#{package}")
   end
 
   desc "update", "updates packages registered to Decker"
   def update
-    exec("babushka.rb 'current dir:update'")
+    system("babushka.rb 'current dir:update'")
   end
 
   desc "patch [FILEPATH]", "registers file at FILEPATH to Decker, so it can be restored"
   def patch(filepath)
-    exec("babushka.rb 'current dir:patch' filepath=#{filepath}")
+    system("babushka.rb 'current dir:patch' filepath=#{filepath}")
   end
 
   desc "restore", "restores all packages and patches to the system"
   def restore
-    exec("babushka.rb 'current dir:restore'")
+    system("babushka.rb 'current dir:restore'")
   end
 end
